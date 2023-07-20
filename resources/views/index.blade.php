4cf22d0c19842faa87e1ba5e2d79111c0b67d633
winnie-fan-0114 <73230465+winnie-fan-0114@users.noreply.github.com>
2023-07-20 15:14:13 +0800

    Update index.blade.php
    試試看

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,7 +17,6 @@
         <input type="text" name="name"><br>
         <input type="radio" name="gender" value="man">男
         <input type="radio" name="gender" value="women">女
-        <input type="radio" name="gender" value="secret">秘密
         <input type="submit" name="submit" value="提交">
     </form>
 @endsection
