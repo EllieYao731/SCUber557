37619bb16b97d970aa046263410929b58d1e120e
yx01300 <nina013020@gmail.com>
2023-07-21 13:28:40 +0800

    首頁

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,15 +1,20 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>@yield('title')</title>
-    </head>
-    <body>
-        <!-- @section('sidebar')
-            這是主要的側邊欄。
-        @show -->
-
-        <div class="container">
-            @yield('content')
-        </div>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <title>index</title>
+    <link rel="stylesheet"  href="style.css">
+</head>
+<body>
+    <div id="title">
+        <h1>SCUber</h1>
+    </div>
+    <br/>
+    <div id="log_in">
+        <button>登入</button>
+    </div>
+    <div id="sign_up">
+        <button>註冊</button>
+    </div>
+</body>
 </html>
