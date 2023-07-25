c0dce2226ff23da3f6e33737a687f150f47ea9a7
yx01300 <nina013020@gmail.com>
2023-07-25 22:48:19 +0800

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,6 @@
-<!-- 繼承模板 -->
 @extends('layouts.layout')
 
 @section('title', 'SCUber577')
-
-<!-- @section('sidebar')
-    @parent
-
-    <p>這邊會附加在主要的側邊欄。</p>
-@endsection -->
 
 @section('content')
 <!DOCTYPE html>
