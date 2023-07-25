b1a6aa1f2a1781a9086cdf0911773fa729aeaedc
gracekuo90 <73097885+gracekuo90@users.noreply.github.com>
2023-07-25 22:29:17 +0800

    新增nav、home(&css)

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,57 @@
+<nav
+	class="navbar navbar-expand-xxl navbar-dark bg-dark"
+	aria-label="Seventh navbar example"
+>
+	<div class="container-fluid">
+		<a class="navbar-brand" href="#">Scuber557</a>
+		<button
+			class="navbar-toggler collapsed"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarsExampleXxl"
+			aria-controls="navbarsExampleXxl"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="navbar-collapse collapse" id="navbarsExampleXxl">
+			<ul class="navbar-nav me-auto mb-2 mb-xl-0">
+				<li class="nav-item">
+					<a class="nav-link active" aria-current="page" href="#">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Link</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link disabled">Disabled</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a
+						class="nav-link dropdown-toggle"
+						href="#"
+						data-bs-toggle="dropdown"
+						aria-expanded="false"
+						>Dropdown</a
+					>
+					<ul class="dropdown-menu">
+						<li><a class="dropdown-item" href="#">Action</a></li>
+						<li><a class="dropdown-item" href="#">Another action</a></li>
+						<li>
+							<a class="dropdown-item" href="#">Something else here</a>
+						</li>
+					</ul>
+				</li>
+			</ul>
+			<form role="search">
+				<input
+					class="form-control"
+					type="search"
+					placeholder="Search"
+					aria-label="Search"
+				/>
+			</form>
+		</div>
+	</div>
+</nav>
