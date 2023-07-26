63e36bb1e83224fe8b9abfbd9ea92394ae960869
yx01300 <nina013020@gmail.com>
2023-07-26 15:10:53 +0800

    update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,35 +3,26 @@
 @section('title', 'SCUber577_login')
 
 @section('style')
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>login</title>
-        <link rel="stylesheet" href="{{ asset('css/style_login.css') }}">
-    </head>
+    <link rel="stylesheet" href="{{asset('css/style_login.css') }}">
+
 @section('content')
-    <body>
-        <div id="container">
-            <div id="title">
-                <h3>請輸入您的學號、密碼</h3>
-            </div>
-            <div id="number">
-                <h3>學號： <input placeholder="請輸入學號"></h3>
-            </div>
-            <div id="password">
-                <h3>密碼：<input type="password"></h3>
-            </div>
-            <div id="forget">
-                <a href="forget.html"><p>忘記密碼？</a></p>
-            </div>
-            <div id="next">
-                <a herf="home.blade.php"><input type="submit" value="下一步"></a>
-            </div>
+    <div id="container">
+        <div id="title">
+            <h3>請輸入您的學號、密碼</h3>
         </div>
-    </body>
+        <div id="number">
+            <h3>學號： <input placeholder="請輸入學號"></h3>
+        </div>
+        <div id="password">
+            <h3>密碼：<input type="password"></h3>
+        </div>
+        <div id="forget">
+            <a href="forget.html"><p>忘記密碼？</a></p>
+        </div>
+        <div id="next">
+            <a herf="home.blade.php"><input type="submit" value="下一步"></a>
+        </div>
+    </div>
     <footer>
         <div id="scu">
             <h1>SCUber<i>577</i></h1>
@@ -40,5 +31,4 @@
             <img src="{{ asset('pictures/people_scooter.png') }}" alt="騎車圖片">
         </div>
     </footer>
-</html>
 @endsection
