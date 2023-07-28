53317834f554874e708a6a413c354227a2a804d6
yx01300 <nina013020@gmail.com>
2023-07-28 14:02:43 +0800

    update

--- resources/views/information.blade.php
+++ resources/views/information.blade.php
@@ -3,46 +3,20 @@
 @section('title', 'SCUber557_signup')
 
 @section('style-black')
-
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <script>
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('select');
+      var instances = M.FormSelect.init(elems);
+    });
+  </script>
 <style>
-#container {
-    display: flex;
-    flex-direction: column;
-    align-items: center; /* 內容垂直居中 */
-}
-
-#container h3 {
-    
-    text-align: left; /* 將內容左對齊 */
-}
-
-#gender {
-    margin-right: 110px;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start; /* 將內容水平左對齊 */
-}
-
-
-#gender h3 {
-    margin-right: 10px; /* 元素之間的間距 */
-}
-
-#tel{
-    margin-right: 30px;
-}
-
-#mail{
-    margin-right: 30px;
-}
-
-#next {
-  margin-top: 50px;
-}
-
-#next:hover {
-    transform: scale(1.5); /* 將按鈕放大 20% */
-}
+#center-content h4{
+        margin-left: 200px;
+        color: white;
+    }
 </style>
 @endsection
 
@@ -50,29 +24,53 @@
 
 @section('content-black')
 
-<div id="container">
-            <div id="title">
-                <h2>請輸入您的基本資料</h2>
+<div class="row">
+    <div class="col s10 offset-s1"> <!-- 使用 .col s6 offset-s3 佈局表單 -->
+      <div id="center-content">
+        <h4>請輸入您的基本資料</h4>
+        <div class="row">
+          <div class="col s8 offset-s2"> <!-- 使用 .col s6 offset-s3 佈局表單 -->
+            <form class="col-sm">
+              <!-- 表單內容 -->
+              <div class="row">
+                <div class="input-field col s12">
+                  <input id="name" type="text" class="validate">
+                  <label for="name">姓名</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <select>
+                    <option value="1">男</option>
+                    <option value="2">女</option>
+                  </select>
+                  <label>性別</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <input id="email" type="email" class="validate">
+                  <label for="email">Email</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <input id="number" type="tel" class="validate">
+                  <label for="number">電話號碼</label>
+                </div>
+              </div>
+            </form>
+            <div class="row"> <!-- 使用 .row 來包含 <div id="next"> -->
+              <div class="col s12"> <!-- 使用 .col s12 佈局 <div id="next"> -->
+                <div id="next">
+                  <a href="#"><input type="submit" value="下一步"></a>
+                </div>
+              </div>
             </div>
-            <div id="name">
-                <h3>姓名： <input type="text"></h3>
-            </div>
-            <div id="gender">
-                <h3>性別：</h3>
-                <select name="gender_select">
-                    <option>男</option>
-                    <option>女</option>
-                </select>
-            </div>
-            <div id = "tel">
-                <h3>手機號碼：<input type="tel"></h3>
-            </div>
-            <div id="mail">
-                <h3>電子郵件：<input type="email"></h3>
-            </div>
-            <div id="next">
-                <a herf=""><input type="submit" value="下一步"></a>
-            </div>
+          </div>
         </div>
+      </div>
+    </div>
+  </div>
 
 @endsection
