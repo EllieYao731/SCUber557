d22aff83e1af6d2e89fe8665ce963e138aa549a5
yx01300 <nina013020@gmail.com>
2023-07-30 23:30:55 +0800

    update scu logo

--- resources/views/information.blade.php
+++ resources/views/information.blade.php
@@ -28,6 +28,46 @@
   white-space: nowrap; /* 防止 .row 元素內容換行 */
 }
 
+@keyframes scu {
+    0%,
+    100% {
+      text-shadow: 0 0 20px #fff, 0 0 100px #fff, 0 0 20px #fff;
+    }
+    50% {
+      text-shadow: 0 0 50px #fff, 0 0 150px #fff, 0 0 50px #fff;
+      color: rgb(255, 75, 75);
+    }
+  }
+  
+  #scu {
+    color: aliceblue;
+    padding: 10px;
+    animation: scu 1.5s ease-in-out infinite alternate;
+  }
+  
+  /* 其他樣式... */
+  
+  .moving-image {
+      position: absolute;
+      bottom: 0;
+      left: 0; /* 將圖片貼在畫面最左邊 */
+      width: 100%;
+      animation: slideAnimation 10s linear infinite;
+    }
+    
+    .moving-image img {
+      width: 100px; /* 調整圖片寬度 */
+      height: auto; /* 高度按比例自動調整 */
+    }
+    
+    @keyframes slideAnimation {
+      0% {
+        transform: translateX(-100%); /* 初始位置，水平偏移量為 -100% */
+      }
+      100% {
+        transform: translateX(100%); /* 結束位置，水平偏移量為 0% */
+      }
+    }
 </style>
 @endsection
 
@@ -83,5 +123,12 @@
       </div>
     </div>
   </div>
-
+  <footer>
+        <div id="scu">
+            <h1>SCUber<i>577</i></h1>
+        </div>
+        <div class="moving-image">
+            <img src="{{ asset('pictures/people_scooter.png') }}" alt="騎車圖片">
+        </div>
+    </footer>
 @endsection
