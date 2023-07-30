93799bc053de6fcfde4271856b53632f11960669
yx01300 <nina013020@gmail.com>
2023-07-31 00:50:47 +0800

    增加忘記密碼頁面

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -61,46 +61,49 @@
 @section('content-black')
 
 <div class="row">
-    <div class="col s12"> <!-- 使用 .col s6 offset-s3 佈局表單 -->
-      <div id="center-content">
-        <h4>請輸入您的學號、密碼</h4>
-        <div class="row">
-          <div class="col s12"> <!-- 使用 .col s6 offset-s3 佈局表單 -->
-            <form class="col-sm">
-              <!-- 表單內容 -->
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="number" type="text" class="validate">
-                  <label for="number">學號</label>
+  <div class="col s12"> <!-- 使用 .col s6 offset-s3 佈局表單 -->
+    <div id="center-content">
+      <h4>請輸入您的學號、密碼</h4>
+      <div class="row">
+        <div class="col s12"> <!-- 使用 .col s6 offset-s3 佈局表單 -->
+          <form class="col-sm">
+            <!-- 表單內容 -->
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="number" type="text" class="validate">
+                <label for="number">學號</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="password" type="password" class="validate">
+                <label for="password">密碼</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <div id="forget">
+                  <a href="forget"><p>忘記密碼？</p></a>
                 </div>
               </div>
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="password" type="password" class="validate">
-                  <label for="password">密碼</label>
-                </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12">
-                  <div id="forget">
-                    <a href="forget"><p>忘記密碼？</p></a> 
-                  </div>
-                </div>
-              </div>
-            </form>
-            <div class="row"> <!-- 使用 .row 來包含 <div id="next"> -->
-              <div class="col s12"> <!-- 使用 .col s12 佈局 <div id="next"> -->
-                <div id="next">
-                  <a href="{{ url('/home') }}"><input type="submit" value="下一步"></a>
-                </div>
+            </div>
+          </form>
+          <div class="row"> <!-- 使用 .row 來包含 <div id="next"> -->
+            <div class="col s12"> <!-- 使用 .col s12 佈局 <div id="next"> -->
+              <div id="next">
+                <a href="{{ url('/home') }}"><input type="submit" value="下一步"></a>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div id="scu">
-      <h1>SCUber<i>577</i></h1>
-    </div>
   </div>
+</div>
+
+<footer>
+  <div id="scu">
+    <h1>SCUber<i>577</i></h1>
+  </div>
+</footer>
 @endsection
