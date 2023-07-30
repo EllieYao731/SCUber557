82d79dfd723a36922478a6ef91a0b1380fe17969
yx01300 <nina013020@gmail.com>
2023-07-31 00:19:57 +0800

    更新模板

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -13,21 +13,7 @@
     });
   </script>
 <style>
-@extends('layouts.black')
 
-@section('title', 'SCUber577_login')
-
-@section('style-black')
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-  <script>
-    document.addEventListener('DOMContentLoaded', function() {
-      var elems = document.querySelectorAll('select');
-      var instances = M.FormSelect.init(elems);
-    });
-  </script>
-  <style>
     #center-content {
       text-align: center;
     }
