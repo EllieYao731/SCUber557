df2f2858de338a660bd763f60e6c894b31677d74
yx01300 <nina013020@gmail.com>
2023-07-31 00:05:11 +0800

    更新模板

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,34 +2,104 @@
 
 @section('title', 'SCUber577_login')
 
-@section('style')
-    <link rel="stylesheet" href="{{asset('css/style_login.css') }}">
+@section('style-black')
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <script>
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('select');
+      var instances = M.FormSelect.init(elems);
+    });
+  </script>
+  <style>
+    #center-content {
+  text-align: center; /* 水平居中 #center-content 內容 */
+}
+
+#center-content h4 {
+  display: inline-block; /* 設置為行內元素，使其保持在同一行 */
+  color: white;
+  margin: 0; /* 清除上下的外邊距 */
+  white-space: nowrap; /* 防止文字換行 */
+}
+
+#center-content .row {
+  white-space: nowrap; /* 防止 .row 元素內容換行 */
+}
+
+#forget {
+  font-size: 15px;
+  position: absolute; /* 將定位方式設為絕對定位 */
+  bottom: 60px;
+  right: 15px;
+}
+
+@keyframes scu {
+    0%,
+    100% {
+      text-shadow: 0 0 20px #fff, 0 0 100px #fff, 0 0 20px #fff;
+    }
+    50% {
+      text-shadow: 0 0 50px #fff, 0 0 150px #fff, 0 0 50px #fff;
+      color: red;
+    }
+  }
+  
+  #scu {
+    color: aliceblue;
+    padding: 10px;
+    animation: scu 1.5s ease-in-out infinite alternate;
+  }
+</style>
+
 @endsection
 
 @section('content')
-    <div id="container">
-        <div id="title">
-            <h3>請輸入您的學號、密碼</h3>
+@section('back-link', '/')
+
+@section('content-black')
+
+<div class="row">
+    <div class="col s12"> <!-- 使用 .col s6 offset-s3 佈局表單 -->
+      <div id="center-content">
+        <h4>請輸入您的學號、密碼</h4>
+        <div class="row">
+          <div class="col s12"> <!-- 使用 .col s6 offset-s3 佈局表單 -->
+            <form class="col-sm">
+              <!-- 表單內容 -->
+              <div class="row">
+                <div class="input-field col s12">
+                  <input id="name" type="text" class="validate">
+                  <label for="name">學號</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <input id="email" type="password" class="validate">
+                  <label for="email">密碼</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <div id="forget">
+                  <a href="forget.html"><p>忘記密碼？</a></p>
+                  </div>
+                </div>
+              </div>
+            </form>
+            <div class="row"> <!-- 使用 .row 來包含 <div id="next"> -->
+              <div class="col s12"> <!-- 使用 .col s12 佈局 <div id="next"> -->
+                <div id="next">
+                  <a href="#"><input type="submit" value="下一步"></a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div id="number">
-            <h3>學號： <input placeholder="請輸入學號"></h3>
-        </div>
-        <div id="password">
-            <h3>密碼：<input type="password"></h3>
-        </div>
-        <div id="forget">
-            <a href="forget.html"><p>忘記密碼？</a></p>
-        </div>
-        <div id="next">
-            <a href="{{ url('/home') }}"><input type="submit" value="下一步"></a>
-        </div>
+      </div>
     </div>
-    <footer>
-        <div id="scu">
+    <div id="scu">
             <h1>SCUber<i>577</i></h1>
-        </div>
-        <div class="moving-image">
-            <img src="{{ asset('pictures/people_scooter.png') }}" alt="騎車圖片">
-        </div>
-    </footer>
+    </div>
 @endsection
