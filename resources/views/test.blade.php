6b59740e963d30f231e4091de0bba8cd62fbf167
yx01300 <nina013020@gmail.com>
2023-09-07 12:47:34 +0800

    影像辨識修改

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -13,12 +13,20 @@
     @isset($imagePath)
         <h3>上傳的圖片：</h3>
         <img src="{{ asset('storage/' . $imagePath) }}" alt="Uploaded Image" width="300">
-        <img src="{{ asset('storage/' . $adjustedImagePath) }}" alt="Uploaded Image" width="300">
+        
+        <h3>調整過的圖片：</h3>
+        <img src="{{ asset('storage/' . $adjustedImagePath) }}" alt="Adjusted Image" width="300">
+
+        <h3>裁切過的圖片：</h3>
+        <img src="{{ asset('storage/' . $croppedImagePath) }}" alt="Cropped Image" width="300">
+        
         @isset($recognizedText)
             <h3>辨識結果：</h3>
+            <p>原始圖片辨識結果：</p>
             <p>{{ $recognizedText }}</p>
+            
+            <p>調整過的圖片辨識結果：</p>
             <p>{{ $adrecognizedText }}</p>
-
         @endisset
     @endisset
 </div>
