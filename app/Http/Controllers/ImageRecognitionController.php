d9a19b9ab2d2566ccb8a4bb26870fb0fc7a732e0
yx01300 <nina013020@gmail.com>
2023-09-07 22:03:35 +0800

    影像辨識範圍修改

--- app/Http/Controllers/ImageRecognitionController.php
+++ app/Http/Controllers/ImageRecognitionController.php
@@ -95,14 +95,15 @@ class ImageRecognitionController extends Controller
         $originalImage = Image::make($cropImagePath);
 
         // 定義裁剪的坐標
-        $x_start = 105;
+        $x_start = 100;
         $x_end = 280;
         $y_start = 120;
         $y_end = 160;
 
         // 裁剪圖像
         $croppedImage = $originalImage->crop($x_end - $x_start, $y_end - $y_start, $x_start, $y_start);
-
+        $croppedImage->greyscale();
+        
         // 儲存調整後的圖像
         $croppedImagePath = 'cropped/cropped_' . basename($imagePath);
         Storage::disk('public')->put($croppedImagePath, $croppedImage->encode());
