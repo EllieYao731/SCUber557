abc86588c869345fc3a2ae9bfc3f6bc3069e8e19
yx01300 <nina013020@gmail.com>
2023-12-05 00:32:02 +0800

    更改性別欄位

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -28,27 +28,38 @@
     #center-content .row {
       white-space: nowrap;
     }
+
+    /* Add the following style rule for the radio buttons */
+    #center-content input[type="radio"].with-gap:checked+span:after {
+      border: 2px solid #fff; /* Set the border color when the radio button is checked */
+    }
+
+    #center-content input[type="radio"].with-gap:checked+span {
+      color: #fff; /* Set the text color when the radio button is checked */
+    }
+
     footer {
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    text-align: center;
-    height: 30px; /* Set the height to match the height of the image */
-    padding-top: 10px; /* Adjust the top padding to give some space above the text */
-    display: flex; /* Use flexbox to center content */
-    justify-content: center; /* Center content horizontally */
-    align-items: center; /* Center content vertically */
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      text-align: center;
+      height: 30px;
+      padding-top: 10px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
     }
+
     @keyframes scu {
-    0%, 100% {
+      0%, 100% {
         text-shadow: 0 0 20px #fff, 0 0 100px #fff, 0 0 20px #fff;
-        color: initial; /* 重置顏色 */
-    }
-    50% {
+        color: initial;
+      }
+      50% {
         text-shadow: 0 0 10px red, 0 0 20px red, 0 0 10px red;
         color: red !important;
+      }
     }
-}
 
     .back-button {
       background-color: #1a1a1a;
@@ -56,24 +67,23 @@
       margin-left: 4%;
       margin-top: 4% !important;
     }
+    
+    #center-content input[type="radio"].with-gap {
+    margin-right: 5px; /* Adjust the margin as needed */
+}
 
     #scu {
       color: aliceblue;
       padding: 10px;
       animation: scu 1.5s ease-in-out infinite alternate;
     }
-    #number, #name, #password, #email {
+
+    #number,
+    #name,
+    #password,
+    #email {
       color: #fff;
     }
-
-    #gender, .select-wrapper .select-dropdown li>span.white-text {
-      color: #fff !important;
-    }
-
-    .input-field select option+ label{
-    color: #fff;
-}
-
   </style>
 @endsection
 
@@ -82,66 +92,76 @@
 @section('content-black')
 
 <div class="row">
-    <div class="col s12">
-      <div id="center-content">
-        <h4>請輸入您的基本資料</h4>
-        <div class="row">
-          <div class="col s12 m8 offset-m2 l6 offset-l3">
-            <form class="col-sm">
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="number" type="text" class="validate">
-                  <label for="number">學號</label>
-                </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="name" type="text" class="validate">
-                  <label for="name">姓名</label>
-                </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="password" type="password" class="validate">
-                  <label for="password">密碼</label>
-                </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12">
-                  <select>
-                    <option value="1" class="white-text">男</option>
-                    <option value="2" class="white-text">女</option>
-                  </select>
-                  <label>性別</label>
-                </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="email" type="email" class="validate">
-                  <label for="email">Email</label>
-                </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="number" type="tel" class="validate">
-                  <label for="number">電話號碼</label>
-                </div>
-              </div>
-            </form>
+  <div class="col s12">
+    <div id="center-content">
+      <h4>請輸入您的基本資料</h4>
+      <div class="row">
+        <div class="col s12 m8 offset-m2 l6 offset-l3">
+          <form class="col-sm">
             <div class="row">
-              <div class="col s12">
-                <div id="next">
+              <div class="input-field col s12">
+                <input id="number" type="text" class="validate">
+                <label for="number">學號</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="name" type="text" class="validate">
+                <label for="name">姓名</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="password" type="password" class="validate">
+                <label for="password">密碼</label>
+              </div>
+            </div>
+            <div class="row">
+    <div class="input-field col s12">
+        <label>性別</label>
+        <p>
+            <label>
+                <input name="gender" type="radio" value="1" class="with-gap" />
+                <span class="white-text">男</span>
+            </label>
+
+            <label>
+                <input name="gender" type="radio" value="2" class="with-gap" />
+                <span class="white-text">女</span>
+            </label>
+        </p>
+    </div>
+</div>
+
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="email" type="email" class="validate">
+                <label for="email">Email</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="number" type="tel" class="validate">
+                <label for="number">電話號碼</label>
+              </div>
+            </div>
+          </form>
+          <div class="row">
+            <div class="col s12">
+              <div id="next">
                 <a href="#"><input type="submit" value="下一步"></a>
-                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
   <footer>
     <div id="scu">
-        <h4>SCUber<i>577</i></h4>
+      <h4>SCUber<i>577</i></h4>
     </div>
   </footer>
+</div>
+
 @endsection
