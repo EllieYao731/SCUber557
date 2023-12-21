5a992db1f1855341fe3393ce66d711cb3c6a9a1e
yx01300 <nina013020@gmail.com>
2023-12-21 16:05:05 +0800

    影像辨識成功！僅限學生證

--- app/Http/Controllers/ImageRecognitionController.php
+++ app/Http/Controllers/ImageRecognitionController.php
@@ -1,11 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use Intervention\Image\ImageManagerStatic as Image;
 use thiagoalessio\TesseractOCR\TesseractOCR;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\Request;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 
@@ -22,126 +19,38 @@ class ImageRecognitionController extends Controller
             'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
 
-        // try {
-        //     // 上傳圖像並取得路徑
-        //     $imagePath = $request->file('image')->store('uploads', 'public');
-        //     $absoluteImagePath = public_path("storage/$imagePath");
-
-        //     // 調整亮度和對比度
-        //     $adjustedImagePath = $this->adjustImage($imagePath);
-        //     $adjustedAbsoluteImagePath = public_path("storage/$adjustedImagePath");
-
-        //     $croppedImagePath = $this->cropImage($adjustedImagePath);
-        //     $croppedAbsoluteImagePath = public_path("storage/$croppedImagePath");
-
         // 上传图像并获取路径
         $imagePath = $request->file('image')->store('uploads', 'public');
         $absoluteImagePath = public_path("storage/$imagePath");
 
         // 调用Python脚本处理图像
-        $pythonScriptPath = public_path('python/a.py');
-        $outputFolder = '../../storage/app/public/processed_images';
+        $pythonScriptPath = public_path('python/test.py');
+        $outputFolder = storage_path('app/public/processed_images');
         if (!file_exists($outputFolder)) {
             mkdir($outputFolder, 0755, true);
         }
 
-        $process = new Process(["python", $pythonScriptPath, $absoluteImagePath, $outputFolder]);
-        $process->run();
-        // // echo $absoluteImagePath;
+        $command = "/Users/liyingxuan/opt/anaconda3/bin/python $pythonScriptPath $absoluteImagePath $outputFolder"; //$pythonScriptPath之前要改自己的python路徑！
+        $cropImagePath = exec($command);
 
-        if (!$process->isSuccessful()) {
-        //     // 获取标准输出和错误输出
-        //     $output = $process->getOutput();
-            $errorOutput = $process->getErrorOutput();
+        // 使用 Laravel 的文件处理函数读取 crop_img 的内容
+        $cropImageContents = file_get_contents($cropImagePath);
 
-        //     // throw new ProcessFailedException($process);
-        //     // echo $output;
-        //     echo "<br/>";
-            echo $errorOutput;
-        }
+        $recognizedText = (new TesseractOCR($absoluteImagePath))
+        ->lang('chi_tra')
+        ->userPatterns('/SCUber577/public/user-patterns.txt')
+        ->psm(3)
+        ->run();
 
+        preg_match_all('/\d+/', $recognizedText, $matches);
+        $numericResults = implode('', $matches[0]);
 
-        $outputImagePath = $outputFolder . '/' . uniqid() . '.jpg';
-
-        // 返回视图，传递处理后的图像路径
-        return view('test', [
-            'imagePath' => $imagePath,
-            'absoluteImagePath' => $absoluteImagePath,
-            'outputImagePath' => $outputImagePath, // 将处理后的图像路径传递给视图
-        ]);
-    }
-
-
-
-            // // 使用 Tesseract OCR 辨識文字
-            // $recognizedText = (new TesseractOCR($absoluteImagePath))
-            //     ->lang('chi_tra')
-            //     ->userPatterns('/SCUber577/public/user-patterns.txt')
-            //     ->psm(6)
-            //     ->run();
-            // $adrecognizedText = (new TesseractOCR($adjustedAbsoluteImagePath))
-            //     ->lang('chi_tra')
-            //     ->userPatterns('/SCUber577/public/user-patterns.txt')
-            //     ->psm(11)
-            //     ->run();
-            // $croppedText = (new TesseractOCR($croppedAbsoluteImagePath))
-            //     ->lang('chi_tra')
-            //     ->userPatterns('/SCUber577/public/user-patterns.txt')
-            //     ->psm(11)
-            //     ->run();
-
-
-            // return view('test', [
-            //     'imagePath' => $imagePath,
-            //     'absoluteImagePath' => $absoluteImagePath,
-                // 'adjustedImagePath' => $adjustedImagePath,
-                // 'croppedImagePath' => $croppedImagePath,
-                // 'recognizedText' => $recognizedText,
-                // 'croppedText' => $croppedText,
-                // 'adrecognizedText' => $adrecognizedText,
-    //         ]);
-    //     } catch (\Exception $e) {
-    //         // 處理異常，例如上傳失敗、圖像處理失敗、OCR 失敗等
-    //         \Log::error('Exception: ' . $e->getMessage());
-
-    //         return view('test', ['error' => $e->getMessage()]);
-    //     }
-    // }
-
-    // private function adjustImage($imagePath)
-    // {
-    //     $absoluteImagePath = public_path("storage/$imagePath");
-    //     $originalImage = Image::make($absoluteImagePath);
-
-    //     // 調整亮度和對比度
-    //     $adjustedImage = $originalImage->brightness(4)->contrast(70);
-
-    //     // 儲存調整後的圖像
-    //     $adjustedImagePath = 'adjusted/adjusted_' . basename($imagePath);
-    //     Storage::disk('public')->put($adjustedImagePath, $adjustedImage->encode());
-
-    //     return $adjustedImagePath;
-    // }
-
-    // private function cropImage($adjustedImagePath)
-    // {
-    //     $absoluteImagePath = public_path("storage/$adjustedImagePath");
-    //     $originalImage = Image::make($absoluteImagePath);
-
-    //     // 定義裁剪的坐標
-    //     $x_start = 110;
-    //     $x_end = 280;
-    //     $y_start = 110;
-    //     $y_end = 158;
-
-    //     // 裁剪圖像
-    //     $croppedImage = $originalImage->crop($x_end - $x_start, $y_end - $y_start, $x_start, $y_start);
-
-    //     // 儲存裁剪後的圖像
-    //     $croppedImagePath = 'cropped/cropped_' . basename($adjustedImagePath);
-    //     Storage::disk('public')->put($croppedImagePath, $croppedImage->encode());
-
-    //     return $croppedImagePath;
-    // }
-
+    return view('test', [
+        'imagePath' => $imagePath,
+        'absoluteImagePath' => $absoluteImagePath,
+        'outputImagePath' => $cropImagePath, 
+        'cropImageContents' => $cropImageContents,
+        'recognizedText' => $numericResults
+]);
+}
 }
