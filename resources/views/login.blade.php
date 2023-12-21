1656b2e273d7b42a3cc95e570ce2536766784e8a
gracekuo90 <73097885+gracekuo90@users.noreply.github.com>
2023-12-21 13:00:19 +0800

    解決學號輸入問題

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -60,7 +60,7 @@
  		background-color: #1a1a1a;
  		padding: 10px;
  		margin-left: 4%;
- 		margin-top: 4% !important; 
+ 		margin-top: 4% !important;
     }
     #scu {
         color: aliceblue;
@@ -96,18 +96,18 @@
 @section('content-black')
 
 <div class="row">
-  <div class="col s12"> 
+  <div class="col s12">
     <div id="center-content">
       <h4>請輸入您的學號、密碼</h4>
       <div class="row">
-        <div class="col s12 m8 offset-m2 l6 offset-l3"> 
+        <div class="col s12 m8 offset-m2 l6 offset-l3">
         <form id="loginForm" class="col-sm" action="api/user/login" method="post">
     @csrf
     <!-- 表單內容 -->
     <div class="row">
         <div class="input-field col s12">
             <input id="number" name="studentID" type="text" class="validate white-text-input" pattern="[0-9]{8}" required>
-            <label for="number">學號</label>
+            <label for="studentID">學號</label>
         </div>
     </div>
     <div class="row">
@@ -131,8 +131,8 @@
       </div>
     </div>
   </form>
-          <!-- <div class="row"> 使用 .row 來包含 <div id="next"> 
-            <div class="col s12">  使用 .col s12 佈局 <div id="next"> 
+          <!-- <div class="row"> 使用 .row 來包含 <div id="next">
+            <div class="col s12">  使用 .col s12 佈局 <div id="next">
               <div id="next">
                 <input type="button" value="下一步" onclick="validateForm()">
               </div>
